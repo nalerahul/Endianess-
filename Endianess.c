b142d0359728ac23220ac1646511c14bfbd6b140
Rahul Nale <31885300+nalerahul@users.noreply.github.com>
2018-12-29 13:23:32 +0530

    Endianess created

---
+++ Endianess.c
@@ -0,0 +1,13 @@
+// Program for Checking Endianess of any Computer //
+#include <stdio.h> 
+int main()  
+{ 
+   unsigned int i = 1; 
+   char *c = (char*)&i; 
+   if (*c)     
+       printf("Little endian"); 
+   else
+       printf("Big endian"); 
+   getchar(); 
+   return 0; 
+} 
